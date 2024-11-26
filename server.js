bd2b8683a250dff3932c3f42c6d9de2d0ba92d7e
Bais26 <baisyufan2004@gmail.com>
2024-11-26 15:36:01 +0700

    deploy on vercel

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res)=> {
+    res.json({
+        message: "Api is Awesome"
+    })
+})
+
 app.get('/api/quote', (req, res) => {
   const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
   res.json(randomQuote);
